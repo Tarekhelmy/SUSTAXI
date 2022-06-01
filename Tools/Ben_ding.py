2995bdab3e0b7b33b1ab51bb8315cf56c4ff1923
4809467 <4809467@student.tudelft.nl>
2022-06-01 09:28:44 +0200

    xvgdl

--- Tools/Ben_ding.py
+++ Tools/Ben_ding.py
@@ -1,14 +1,25 @@
 import matplotlib.pyplot as plt
-#from Wing_box_adsee import x_mid, y_mid, wing_box
+from Wing_box_adsee import y_dist
 from Wing_Calculator import lift, comp_halfdata, trailingedgeangle, leadingedgeangle, c_r
 import numpy as np
 #print((lift()))
 #print(-1*comp_halfdata[:,0])
 
+n_safety=1.5
+
+# ======= Materials =======
+# Aluminium 7075
+E_al7075 = 71.7 # GPa
+UTS_al7075 = 572 # MPa
+YTS_al7075 = 503 # MPa
+poisson_al7075 = 0.33
+G_al7075 = 26.9 # GPa
+#ShS_al7075 =
+
 def Mz():
     Mz = abs(comp_halfdata[1:,0]) * lift()
     Mn = ()
-    for i in range(0,len(Mz),1):
+    for i in range(0,len(Mz)+1,1):
         Mn += (sum(Mz[:i]),)
 
     return Mn
@@ -18,18 +29,30 @@ def Mz():
 
 "calculating chord at x coordinates"
 def chord():
-    x_coords = -comp_halfdata[:,0]
+    x_coords = -comp_halfdata[1:,0]
     return c_r() - (x_coords*(np.tan(trailingedgeangle())+np.tan(leadingedgeangle())))
 
 print(chord())
-"required I_yy at each point"
+"required I_yy at each point along span"
+yy = chord()*y_dist
+mz = np.array(Mz())
+sigma = 1.5*YTS_al7075*10**6
+i_yy = mz[1:]*yy/sigma
+
+print("required I_yy in m4 * 10^6",i_yy*10**6)
+
+"I_yy of a single stringer"
+t_string = 10
+l_string = 100
 
 
-# ======= Materials =======
-# Aluminium 7075
-E_al7075 = 71.7 # GPa
-UTS_al7075 = 572 # MPa
-YTS_al7075 = 503 # MPa
-poisson_al7075 = 0.33
-G_al7075 = 26.9 # GPa
-ShS_al7075 =
+"deflection with this I_yy"
+
+
+
+
+
+"buckling"
+
+
+
