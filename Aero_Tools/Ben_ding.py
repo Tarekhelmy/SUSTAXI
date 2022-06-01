21570fceec7ea89993ac63fcb4dd69d1c0f63efe
4809467 <4809467@student.tudelft.nl>
2022-06-01 17:06:51 +0200

    xvgdl

--- Aero_Tools/Ben_ding.py
+++ Aero_Tools/Ben_ding.py
@@ -4,7 +4,8 @@ import numpy as np
 #print((lift()))
 #print(-1*comp_halfdata[:,0])
 
-n_safety=1.5 * 4.6
+n_safety=1.5
+n_load = 4.6
 
 # ======= Materials =======
 # Aluminium 7075
@@ -22,7 +23,7 @@ poisson_al7050 = 0.33
 G_al7050 = 26.9 # GPa
 
 def Mz():
-    Mz = (abs(comp_halfdata[1:,0])+ abs(comp_halfdata[:-1,0]))/2 * lift()
+    Mz = (abs(comp_halfdata[1:,0])+ abs(comp_halfdata[:-1,0]))/2 * (lift()*n_load)
     Mn = ()
     for i in range(0,len(Mz)+1,1):
         Mn += (sum(Mz[:i]),)
@@ -43,14 +44,14 @@ def chord():
 "Bending required I_yy at each point along span"
 yy = chord()*y_dist
 mz = np.array(Mz())
-sigma = n_safety*YTS_al7050*10**6
+sigma = YTS_al7050/n_safety*10**6
 i_yy = (mz[1:]*yy)/sigma
 
 
 scaled = wing_box(0.15, 0.7)[0] * ((chord())**2)
 print(scaled)
 
-print("required I_yy",i_yy - scaled)
+req_I_yy = i_yy - scaled
 
 "buckling"
 
@@ -66,31 +67,35 @@ def buckling():
 t_string = 10
 l_string = 100
 
-# Stringers
-# L-shape
+I_yy_box, Upper_sheet, Lower_sheet = wing_box(0.15, 0.7)
+
+box_centre = np.average(Upper_sheet[1] + Lower_sheet[1])
+
+# Stringers (L-shape)
 L_base = 100 # mm
 L_web = 100 # mm
 t_str_L = 3 # mm
+A_str_L = L_base*t_str_L + L_web*t_str_L
 cent_y_str_L = (L_base * t_str_L * t_str_L/2 + L_web * t_str_L * L_web/2)/(L_base * t_str_L + L_web * t_str_L)
 I_yy_str_L = t_str_L*L_web**3/12 + (L_web/2 - cent_y_str_L)**2 * L_web*t_str_L \
-           + (cent_y_str_L - t_str_L/2)**2 * L_base*t_str_L
+             + (cent_y_str_L - t_str_L/2)**2 * L_base*t_str_L
+
 
 # Z-shape
 Z_base = 50     # mm
 Z_web = 100     # mm
 Z_top = 50      # mm
 t_str_Z = 3       # mm
+A_str_Z = Z_base*t_str_Z + Z_web*t_str_Z + Z_top*t_str_Z
 cent_y_str_Z = Z_web/2
-I_yy_str_L_Z = t_str_Z*Z_web**3/12 \
-            + (cent_y_str_Z - t_str_Z/2)**2 * (Z_base + Z_top) *t_str_Z
+I_yy_str_Z = t_str_Z*Z_web**3/12 \
+             + (cent_y_str_Z - t_str_Z/2)**2 * (Z_base + Z_top) *t_str_Z
 
-"deflection with this I_yy"
+# Add Stringers
+I_yy_four_corner_str = 4*I_yy_str_L \
+                       + A_str_L*((Upper_sheet[1][0]-box_centre)**2 + (Upper_sheet[1][-1]-box_centre)**2 +
+                                  (Lower_sheet[1][0]-box_centre)**2 + (Lower_sheet[1][-1]-box_centre)**2) * (chord()*1000)**2
 
-
-
-
-
-"buckling"
-
+print(req_I_yy - (I_yy_four_corner_str/10**12))
 
 
