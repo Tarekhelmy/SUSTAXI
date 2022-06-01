4da51e1099bda8233640cb122f4b025664ccf1b2
jsvensson <j.b.svensson@student.tudelft.nl>
2022-06-01 14:23:11 +0200

    Removed the duplicated files for Aerodynamics

--- Aero_Tools/Ben_ding.py
+++ Aero_Tools/Ben_ding.py
@@ -1,5 +1,4 @@
-import matplotlib.pyplot as plt
-from Wing_box_adsee import y_dist
+from Aero_Tools.Wing_box_adsee import y_dist
 from Wing_Calculator import lift, comp_halfdata, trailingedgeangle, leadingedgeangle, c_r
 import numpy as np
 #print((lift()))
@@ -45,6 +44,23 @@ print("required I_yy in m4 * 10^6",i_yy*10**6)
 t_string = 10
 l_string = 100
 
+# Stringers
+# L-shape
+L_base = 100 # mm
+L_web = 100 # mm
+t_str_L = 3 # mm
+cent_y_str_L = (L_base * t_str_L * t_str_L/2 + L_web * t_str_L * L_web/2)/(L_base * t_str_L + L_web * t_str_L)
+I_yy_str_L = t_str_L*L_web**3/12 + (L_web/2 - cent_y_str_L)**2 * L_web*t_str_L \
+           + (cent_y_str_L - t_str_L/2)**2 * L_base*t_str_L
+
+# Z-shape
+Z_base = 50     # mm
+Z_web = 100     # mm
+Z_top = 50      # mm
+t_str_Z = 3       # mm
+cent_y_str_Z = Z_web/2
+I_yy_str_L_Z = t_str_Z*Z_web**3/12 \
+            + (cent_y_str_Z - t_str_Z/2)**2 * (Z_base + Z_top) *t_str_Z
 
 "deflection with this I_yy"
 
