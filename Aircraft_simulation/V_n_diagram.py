c8ca0a5471e010b0085e0058c3cec7386f6c502c
theofalat <80906337+theofalat@users.noreply.github.com>
2022-06-07 11:44:55 +0200

    Added a print statement for the negative load factor airspeed

--- Aircraft_simulation/V_n_diagram.py
+++ Aircraft_simulation/V_n_diagram.py
@@ -49,6 +49,7 @@ class VNDiagram(Stability):
         # Taken from CS-23
 
         VS = np.sqrt((-self.negloadfactor * self.W_S) / (0.5 * self.density * self.CLmax_land))
+        print(f"Negative load factor airspeed is: {VS}")
         # This occurs at the negative load factor
 
         negativeloading = [-0.5 * self.density * V ** 2 * self.CLmax_land / self.W_S for V in np.linspace(0, VS, 500)]
