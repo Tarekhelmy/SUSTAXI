16b42a7d5379e47c751b91c5d70cda721d5646a4
Maarten <maartenkrikke@gmail.com>
2022-06-07 16:44:19 +0200

    cleaned up some trash

--- Aircraft_simulation/mass_estimations.py
+++ Aircraft_simulation/mass_estimations.py
@@ -1,8 +1,5 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from scipy.stats import linregress
 from Wing_Power_Loading import WingAndPowerSizing
-from fuel_cell_optimization import FuelCellSizing
 from barchart import *
 
 
